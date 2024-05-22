8ae8b0fcbd88aa51a5e885b65b785186d6bc54ac
Caliche Orozco <caliche2da1@gmail.com>
2024-05-22 16:34:19 -0600

    Update Shop component to reflect construction status

--- src/app/section/Shop.jsx
+++ src/app/section/Shop.jsx
@@ -4,7 +4,7 @@ function Shop () {
     <section className='bg-black px-4 md:px-12'>
       <div className='max-w-6xl mx-auto'>
         <h2 className='text-3xl md:text-4xl font-bold text-teal-400'>
-          Coming Soon...
+          The Shop is under construction you still can visit our store at
         </h2>
       </div>
     </section>

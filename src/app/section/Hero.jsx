88c816f2fc5807a95f8f59768f2eec4f2628db92
Caliche Orozco <caliche2da1@gmail.com>
2024-05-22 14:42:17 -0600

    refactor: change order

--- src/app/section/Hero.jsx
+++ src/app/section/Hero.jsx
@@ -7,18 +7,6 @@ export default function Hero () {
   const colors = ['#20C997', '#FF8C42', '#17A2B8', '#6F42C1']
   return (
     <div className='flex flex-col md:flex-row justify-around items-center w-full'>
-      {/* Text container */}
-      <div className='flex flex-col gap-y-4'>
-        <RoughNotationGroup show={true}>
-          {['Harder', 'Better', 'Faster', 'STRONGER'].map((text, idx) => (
-            <RainbowHighlight key={idx} color={colors[idx]}>
-              <h1 className='font-roboto text-4xl mx-4 md:text-6xl xl:text-7xl font-bold text-white'>
-                {text}
-              </h1>
-            </RainbowHighlight>
-          ))}
-        </RoughNotationGroup>
-      </div>
       {/* Image container */}
       <div>
         <div className='relative w-3/4 mx-auto'>
@@ -33,6 +21,18 @@ export default function Hero () {
             <div className='flex flex-row space-x-4'></div>
           </div>
         </div>
+      </div>
+      {/* Text container */}
+      <div className='flex flex-col gap-y-4'>
+        <RoughNotationGroup show={true}>
+          {['Harder', 'Better', 'Faster', 'STRONGER'].map((text, idx) => (
+            <RainbowHighlight key={idx} color={colors[idx]}>
+              <h1 className='font-roboto text-4xl mx-4 md:text-6xl xl:text-7xl font-bold text-white'>
+                {text}
+              </h1>
+            </RainbowHighlight>
+          ))}
+        </RoughNotationGroup>
       </div>
     </div>
   )
